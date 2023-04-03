16a0302b6524825107e90b6e15a6f1837ebc31fc
Ryan Defryan <muhammaddefryan@gmail.com>
2023-04-03 22:26:14 +0700

    Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "./projects/server/src/index.js",
       env: {
         NODE_ENV: "production",
-        PORT: 8201,
+        PORT: 8302,
       },
       time: true,
     },
