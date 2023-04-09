9cb49bc060c7d338b1e1db4624e3febe1776a9e8
Ryan Defryan <muhammaddefryan@gmail.com>
2023-04-10 00:16:29 +0700

    Update index.js

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/public', express.static(path.join(__dirname, '/Public/images')))
+app.use('/images', express.static(path.join(__dirname, '/images')))
 
 //#region API ROUTES
 const {adminRouter,userRouter,productRouter,transactionRouter,
