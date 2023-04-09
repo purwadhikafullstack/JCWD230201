6444ac021164d4df4a70c09c7feb43620e7ac826
Ryan Defryan <muhammaddefryan@gmail.com>
2023-04-09 13:41:35 +0700

    Update index.js

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -8,10 +8,9 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
 
-
 app.use(express.json());
 
-app.use('/Public/', (path.join(__dirname, '/Public')))
+app.use('/Public/', express.static(path.join(__dirname, '/Public')))
 
 //#region API ROUTES
 const {adminRouter,userRouter,productRouter,transactionRouter,
