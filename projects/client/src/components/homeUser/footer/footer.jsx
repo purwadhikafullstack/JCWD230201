81455a88ae9750a8dca8ac976b245f6685845776
vero8090 <rochan.sm@gmail.com>
2023-04-09 11:47:21 +0700

    fixing bug

--- projects/client/src/components/homeUser/footer/footer.jsx
+++ projects/client/src/components/homeUser/footer/footer.jsx
@@ -57,9 +57,7 @@ export default function Footer() {
 
             <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 px-5 md:px-10 lg:px-60 lg:gap-10 pb-10 border-t h-max pt-5 bg-white inset-x-0 bottom-auto">
                 <div className="">
-                    <div className="text-lg font-semibold">
-                    <img src={`${process.env.REACT_APP_API_IMAGE_URL}Public/images/F_logo.png`} alt="" width={'80px'} />
-                    </div>
+                <img src={`${process.env.REACT_APP_API_IMAGE_URL}Public/images/F_logo.png`} alt="" className='bg-black' width={'130px'} />
                     <div className="text-neutral-800 text-sm">
                         <p className="leading-loose">
                             iFrit is a leading Apple Premium Reseller in Indonesia specializing in Apple products and a wide range of complementary accessories, software and other products.
