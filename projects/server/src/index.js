f662c572702edf68281b33df8378df8ea1a4c064
Ryan Defryan <muhammaddefryan@gmail.com>
2023-04-09 13:46:15 +0700

    Update index.js

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -2,6 +2,7 @@ require("dotenv/config");
 const express = require("express");
 const cors = require('cors');
 const { join } = require("path");
+const path = require("path");
 require("dotenv/config")
 
 const PORT = process.env.PORT || 8000;
