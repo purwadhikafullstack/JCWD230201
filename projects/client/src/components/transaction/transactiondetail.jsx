4eb11daddb9c46a64227e733dbdda0989f850a9c
Ryan Defryan <muhammaddefryan@gmail.com>
2023-04-10 12:05:26 +0700

    Update transactiondetail.jsx

--- projects/client/src/components/transaction/transactiondetail.jsx
+++ projects/client/src/components/transaction/transactiondetail.jsx
@@ -47,7 +47,7 @@ export default function TransactionDetail() {
                         <div className='fixed z-50 p-10 w-screen h-screen flex flex-col items-center'>
                             <div className='p-5 bg-white lg:w-1/3 w-1/2 h-1/2 xl:h-full'>
                                 <button onClick={() => setMaxpayment(false)}><AiOutlineClose size={'20px'} /></button>
-                                <img src={`http://localhost:8000/${transaction.upload_payment}`} className='w-full h-full object-contain overflow-y-auto bg-gray-200 bg-opacity-20' alt="" />
+                                <img src={`https://jcwd230201.purwadhikabootcamp.com/${transaction.upload_payment}`} className='w-full h-full object-contain overflow-y-auto bg-gray-200 bg-opacity-20' alt="" />
                             </div>
                         </div> : null : null
             }
