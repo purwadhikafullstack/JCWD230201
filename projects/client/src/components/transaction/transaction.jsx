4caa1db70eaf9fd4c4690747aab72038f5dd0d2f
Ryan Defryan <muhammaddefryan@gmail.com>
2023-04-13 15:04:36 +0700

    Deployment - 2023/04/13 15:03 (#14)
    * SetupProject_1st push
    
    * HomePage_1st-Push
    
    * Move folder sequelize to src server
    
    * Moving Sequelize folder to src server
    
    * add headless ui
    
    * Login admin 1st push
    
    * update2
    
    * Update Navbar
    
    * Admin Login Done
    
    * Upload Register
    
    * FE-Show All Products Done
    
    * BE-Show All Products Done
    
    * FE-Show Product Details
    
    * BE-Show Product Details Done
    
    * BE-Able to register as user Done
    
    * FE-Able to register as User Done
    
    * Update Spinner and Toast Done
    
    * Disable non user and admin added, all fitur done
    
    * added error page, admin can asign user admin to WH
    
    * Bug Port ProductDetail Done
    
    * adding moment library in server
    
    * 1st Push_adding menu dropdown
    
    * first push
    
    * adding hero icons in react
    
    * adding transaction BE, modify client
    
    * add some new
    
    * fixing bug nodemon error
    
    * mapping transaction from users
    
    * User can verify account done
    
    * Update activation.jsx
    
    * filter transaction based WH and WH admin done
    
    * Add, Change, New Address Done
    
    * Add Router for Index
    
    * adding axios in BE
    
    * BE-User can login
    
    * FE-User can login
    
    * User can Reset Password Done
    
    * Add Confirm Email
    
    * adding open cage
    
    * first adding warehouse
    
    * Admin can see, edit, and delete
    
    * fixing bug, wh admin still can go to warehouse edit
    
    * Update Personal Data
    
    * Keep Login Updated
    
    * User can update personal data Update
    
    * fixing bug editing data admin
    
    * adding multer
    
    * User can update data Update
    
    * User Can Update Data Done
    
    * Update Data
    
    * Change Shipping Done
    
    * Cannot Do Any Sort Whil Not Registered Done
    
    * 1st add sales report
    
    * adding react moment
    
    * 2nd update
    
    * User can add address Done
    
    * 3rd edit
    
    * 4th update
    
    * Add to Cart Done
    
    * sales reports filter month and WH done
    
    * update ui, fixing bug searching, adding some transaction
    
    * Update Cart Done
    
    * Progress New Order
    
    * Update Button Done
    
    * Creating New Order Done
    
    * Update Cart
    
    * Update Cart
    
    * Update
    
    * adding moment,datepicker,lottiefiles
    
    * update1
    
    * Arifitanto_MWE-24_ManageProductAdminDone
    
    * Update Cart Latest Update
    
    * ui update, adding filter date transaction
    
    * Update Cart Added Notif Latest
    
    * Create New Order Done
    
    * Transaction History Added
    
    * adding automatical mutation
    
    * update ui, create button submit, BE submit
    
    * Admin Update Product Stock Done
    
    * Upload Payment Done
    
    * Order List Done
    
    * Admin Cancel Order Done
    
    * Updated
    
    * update and fixing transaction, cancel done
    
    * fixing create id transaction
    
    * Done
    
    * Updated
    
    * adding react select
    
    * Arifitanto MWE-26 Stock Mutation Done
    
    * Log Stock done, and few update
    
    * fixing bug BE
    
    * fixing bug
    
    * changing API FE
    
    * Testing-05
    
    * Additional Added
    
    * Additional Added
    
    * Console Commented
    
    * debugging , responsive not yet
    
    * Additional Fixing Bug-2
    
    * Bug Fixing 1
    
    * Bug Fixing Picture
    
    * Update ui, adding dashboard, admin responsive not yet
    
    * Fixing Pagination User Done
    
    * debugging, changing icon
    
    * Bug Fixing 2
    
    * fixing bug
    
    * Update userController.js
    
    * fix bug admin
    
    * chaging localhost:8000 with env
    
    * dashboard nan fix
    
    * Bug Fixing 4
    
    * MiniNavbar Done
    
    * add to cart fix
    
    * Bug Fixing 5
    
    * Bug Fixing Product Done
    
    * admin responsive done, submit disable done
    
    * fixing merge
    
    * add
    
    * changing name ifrit
    
    * fixing bug transaction page
    
    ---------
    
    Co-authored-by: vero8090 <rochan.sm@gmail.com>
    Co-authored-by: R. Savero <116779928+vero8090@users.noreply.github.com>
    Co-authored-by: Raihan162 <muhammadraihanarrafi@gmail.com>
    Co-authored-by: Arifitanto Oktarian Hafayer <arifitantotito@gmail.com>

--- projects/client/src/components/transaction/transaction.jsx
+++ projects/client/src/components/transaction/transaction.jsx
@@ -438,7 +438,7 @@ export default function Transaction() {
                                         Page {listData.slot} of {listData.total_pages}
                                     </div>
                                     <button
-                                        disabled={(listData.page + 1) > listData.total_pages || listData.loading}
+                                        disabled={(listData.slot + 1) > listData.total_pages}
                                         onClick={() => {
                                             setListData({ ...listData, loading: true })
                                             getTr(pickWH, pickStatus, selectedDate.from, selectedDate.to, listData.slot + 1)
